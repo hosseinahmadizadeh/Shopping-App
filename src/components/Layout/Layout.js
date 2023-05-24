b92745e122e776db32b27004cd3da6126df70d90
Hossein <hosseinahmadizadeh1377@gmail.com>
2023-05-24 12:23:40 +0330

    Adding Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Wrapper from "../../hoc/Wrapper";
 import "./Layout.css";
+import Navbar from "../Navigation/Navbar/Navbar";
 
 const Layout = (props) => {
   return (
     <Wrapper>
-      <div>Navigation</div>
+      <Navbar />
       <main className="content">{props.children}</main>
     </Wrapper>
   );
---
+++ src/components/Navigation/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = (props) => {
+  return (
+    <header className="navbar">
+      <nav>...</nav>
+    </header>
+  );
+};
+
+export default Navbar;
