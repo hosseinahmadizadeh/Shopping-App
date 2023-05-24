6ad4af15f7ae495a915af2eda2d6de044317eb8e
Hossein <hosseinahmadizadeh1377@gmail.com>
2023-05-24 14:16:17 +0330

    Navigation items

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import NavItems from "../NavItems/NavItems";
 
 const Navbar = (props) => {
   return (
     <header className="navbar">
-      <nav>...</nav>
+      <nav>
+        <NavItems />
+      </nav>
     </header>
   );
 };
